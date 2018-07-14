5066ae7b82eeeaedfd77db5fcbed57a202031887
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-07-14 11:50:38 +0200

    add graphics, fonts, unit selection and menu bar

--- project/units/Unit.gd
+++ project/units/Unit.gd
@@ -1,5 +1,23 @@
 extends Node
 
-var name
+export var selected = false
+onready var box = $SelectionBox
+
+var unitname
 var life
 var attack
+
+func _process(delta):
+	if selected and box.visible == false:
+		box.visible = true
+	else:
+		if box.visible:
+			box.visible = false
+
+func _on_unit_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == BUTTON_LEFT:
+			selected = true
+		if event.button_index == BUTTON_RIGHT:
+			selected = false
+	pass
